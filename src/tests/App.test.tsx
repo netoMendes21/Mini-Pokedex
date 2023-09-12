156acdc2b1901c881549092c7753d5f5c801ec7b
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-09-12 19:28:54 -0300

    Terceiro requisito finalizado

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,20 +2,26 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('O primeiro link deve ter o texto Home', () => {
-  renderWithRouter(<App />);
-  const linkHome = screen.getByRole('link', { name: 'Home' });
-  expect(linkHome).toBeInTheDocument();
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+  test('O primeiro link deve ter o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  test('O segundo link deve ter o texto About', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve ter o texto Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    expect(linkFavoritePokemon).toBeInTheDocument();
+  });
 });
 
-test('O segundo link deve ter o texto About', () => {
-  renderWithRouter(<App />);
-  const linkAbout = screen.getByRole('link', { name: 'About' });
-  expect(linkAbout).toBeInTheDocument();
-});
+test('Teste se a aplicação é redirecionada para a página inicial, na URL /, ao clicar no link Home da barra de navegação.', () => {
 
-test('O terceiro link deve ter o texto Favorite Pokémon', () => {
-  renderWithRouter(<App />);
-  const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
-  expect(linkFavoritePokemon).toBeInTheDocument();
 });
--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,30 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('É exibida na tela a mensagem No favorite pokémon found caso a pessoa não tenha Pokémon favorito.', () => {
+  renderWithRouter(<App />, { route: '/favorites' });
+
+  const notFavorite = screen.getByText('No favorite Pokémon found');
+  expect(notFavorite).toBeVisible();
+});
+
+test('Apenas são exibidos os Pokémon favoritados', async () => {
+  renderWithRouter(<App />);
+
+  const linkParaDetalhes = screen.getByRole('link', { name: /More details/i });
+  expect(linkParaDetalhes).toBeInTheDocument();
+  await userEvent.click(linkParaDetalhes);
+
+  const marcado = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+  await userEvent.click(marcado);
+  expect(marcado).toBeInTheDocument();
+
+  const favoritado = screen.getByRole('link', { name: /Favorite Pokémon/i });
+  expect(favoritado).toBeInTheDocument();
+  await userEvent.click(favoritado);
+
+  const mostrar = screen.getByText(/Pikachu/i);
+  expect(mostrar).toBeInTheDocument();
+});
