2cd7cea62966792e99a3545f0bce2fa87f0702cb
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-09-12 15:19:35 -0300

    Primeiro requisito finalizado

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('O primeiro link deve ter o texto Home', () => {
+  renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: 'Home' });
+  expect(linkHome).toBeInTheDocument();
+});
+
+test('O segundo link deve ter o texto About', () => {
+  renderWithRouter(<App />);
+  const linkAbout = screen.getByRole('link', { name: 'About' });
+  expect(linkAbout).toBeInTheDocument();
+});
+
+test('O terceiro link deve ter o texto Favorite Pokémon', () => {
+  renderWithRouter(<App />);
+  const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  expect(linkFavoritePokemon).toBeInTheDocument();
+});
